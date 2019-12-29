a34cb0fc98bf4be4dda50dd01bfe76a393f09cb7
elahefth <43049353+elahefth@users.noreply.github.com>
2019-12-29 20:15:39 +0330

    add marginTop

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     ...StyleSheet.absoluteFillObject,
+    marginTop: 180
   },
   finder: {
     alignItems: 'center',
